542756beefab2f85880dd031ecb11d4fb45d6665
Atin Singh <atin@pragra.co>
2019-02-02 13:57:58 -0500

    Added config

--- src/test/java/co/pragra/b10/framework/config/DriverConfig.java
+++ src/test/java/co/pragra/b10/framework/config/DriverConfig.java
@@ -1,4 +1,35 @@
 package co.pragra.b10.framework.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class DriverConfig {
+
+    private static Properties properties;
+
+    private DriverConfig(){
+        properties = new Properties();
+        Path path = Paths.get("src","test", "resources","driverconfig.properties");
+
+        try {
+
+            InputStream fileInputStream = new FileInputStream(path.toFile());
+            properties.load(fileInputStream);
+
+        }catch (IOException ex){
+
+        }
+    }
+
+
+    public static String getProperty(String key){
+        if(properties==null){
+            new DriverConfig();
+        }
+        return (String)properties.get(key);
+    }
 }
--- src/test/java/co/pragra/b10/framework/drivermanagement/DriverManager.java
+++ src/test/java/co/pragra/b10/framework/drivermanagement/DriverManager.java
@@ -1,7 +1,9 @@
 package co.pragra.b10.framework.drivermanagement;
 
+import co.pragra.b10.framework.config.DriverConfig;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
 
@@ -10,8 +12,20 @@ public class DriverManager {
 
 
     private DriverManager(){
-        System.setProperty("webdriver.chrome.driver","/Users/atinsingh/Downloads/chromedriver");
-        this.driver = new ChromeDriver();
+
+        if(DriverConfig.getProperty("browser").equals("chrome")){
+            System.setProperty("webdriver.chrome.driver", DriverConfig.getProperty("chrome-driver-path"));
+            this.driver = new ChromeDriver();
+        }else
+        if(DriverConfig.getProperty("browser").equals("firefox")){
+            System.setProperty("webdriver.gecko.driver", DriverConfig.getProperty("gecko-driver-path"));
+            this.driver = new FirefoxDriver();
+        }else {
+            System.setProperty("webdriver.chrome.driver", DriverConfig.getProperty("chrome-driver-path"));
+            this.driver = new ChromeDriver();
+        }
+
+
     }
 
     public static WebDriver getDriverInstance(){
